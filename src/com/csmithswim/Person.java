4860e2df39cb26210d6fbc74c3b15cec8a619367
Cullen S <cullensmi@gmail.com>
2020-12-03 12:00:59 -0500

    Solved algorithm

--- src/com/csmithswim/Person.java
+++ src/com/csmithswim/Person.java
@@ -1,30 +1,31 @@
 package com.csmithswim;
 import java.io.*;
 import java.util.*;
-
 public class Person {
     private int age;
 
     public Person(int initialAge) {
 
-        age = initialAge;
+
 
         // Add some more code to run some checks on initialAge
 
-        if (age == 0) {
+        if (initialAge <= 0) {
             age = 0;
             System.out.println("Age is not valid, setting age to 0.");
+        } else {
+            age = initialAge;
         }
     }
 
     public void amIOld() {
         // Write code determining if this person's age is old and print the correct statement:
         if (age < 13) {
-            System.out.println("You are young.");
+            System.out.print("You are young.");
         } else if (age >= 13 && age < 18) {
-            System.out.println("You are a teenager.");
+            System.out.print("You are a teenager.");
         } else {
-            System.out.println("You are old.");
+            System.out.print("You are old.");
         }
         System.out.println(/*Insert correct print statement here*/);
     }
@@ -34,3 +35,19 @@ public class Person {
         age++;
     }
 }
+/*
+    4
+    -1
+    10
+    16
+    18
+    Age is not valid, setting age to 0.
+    You are young.
+    You are young.
+    You are young.
+    You are a teenager.
+    You are a teenager.
+    You are old.
+    You are old.
+    You are old.
+ */
